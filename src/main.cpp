45e4d60300a1cd398af6116631d582036f1fe4ed
Pawluk_pc_wsl <spychala.paw@gmail.com>
2024-03-17 12:42:09 +0100

    Zmieniłem main.cpp na kod z roznymi caseami ale tylko z funkcją sort i działa

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,61 @@
 #include <iostream>
 
-#include "hello_world.h"
+#include <vector>
 
-#include "insertion_sort.h"
+#include <algorithm>
 
-int main() {
-    std::cout << moja_funkcja();
+std::vector<int> sort_using_algorithm(std::vector<int> &data, int algorithm)
+{
+    // This function takes the algorithm identifier as input
+    // However, it always uses the sorted function in C++
+    switch (algorithm)
+    {
+    case 1:
+        std::sort(data.begin(), data.end());
+        break;
+    
+    case 2:
+        std::sort(data.begin(), data.end());
+        reverse(data.begin(), data.end());
+        break;
+
+    default:
+        std::sort(data.begin(), data.end());
+        break;
+    }
+    
+    return data;
+}
+
+int main(int argc, char *argv[])
+{
+    // Command-line arguments: ./a.out --algorithm <algorithm_number>
+    if (argc != 3 || std::string(argv[1]) != "--algorithm")
+    {
+        std::cout << "Usage: ./a.out --algorithm <algorithm_number>\n";
+        return 1;
+    }
+
+    int algorithm_number = std::stoi(argv[2]);
+
+    // Read input data from standard input until the end of file (EOF)
+    std::vector<int> data;
+    int num;
+    while (std::cin >> num)
+    {
+        data.push_back(num);
+    }
+
+    // Perform sorting using the specified algorithm (ignored in this example)
+    std::vector<int> sorted_data = sort_using_algorithm(data, algorithm_number);
+
+    // Print the sorted data
+    std::cout << "Sorted data:";
+    for (int i = 0; i < std::min(10, static_cast<int>(sorted_data.size())); ++i)
+    {
+        std::cout << " " << sorted_data[i];
+    }
+    std::cout << std::endl;
+
     return 0;
 }
